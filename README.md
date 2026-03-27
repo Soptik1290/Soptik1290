36e9525163d2ea343817c2836cc8c6cd41de7578
Tomáš Stark <soptik1290game@seznam.cz>
2026-03-27 14:45:40 +0100

    Personalize profile README: update user info, fix layouts and configure GitHub actions

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-<a href="https://github.com/Andrew6rant/Andrew6rant">
+<a href="https://github.com/Soptik1290/Soptik1290">
   <picture>
     <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/Soptik1290/Soptik1290/main/dark_mode.svg">
     <img alt="Soptik1290's Github Stats" src="https://raw.githubusercontent.com/Soptik1290/Soptik1290/main/light_mode.svg">
--- today.py
+++ today.py
@@ -385,12 +385,7 @@ if __name__ == '__main__':
     contrib_data, contrib_time = perf_counter(graph_repos_stars, 'repos', ['OWNER', 'COLLABORATOR', 'ORGANIZATION_MEMBER'])
     follower_data, follower_time = perf_counter(follower_getter, USER_NAME)
 
-    if OWNER_ID == {'id': 'MDQ6VXNlcjU3MzMxMTM0'}:
-        archived_data = add_archive()
-        for index in range(len(total_loc)-1):
-            total_loc[index] += archived_data[index]
-        contrib_data += archived_data[-1]
-        commit_data += int(archived_data[-2])
+
 
     for index in range(len(total_loc)-1): total_loc[index] = '{:,}'.format(total_loc[index])
 
